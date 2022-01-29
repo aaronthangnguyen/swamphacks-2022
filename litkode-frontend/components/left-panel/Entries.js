421b327adacaa6898e7d319e900dee4ee75633bf
Aaron Nguyen <me@aaronnguyen.dev>
2022-01-29 15:27:34 -0500

    Populate mockup

--- litkode-frontend/components/left-panel/Entries.js
+++ litkode-frontend/components/left-panel/Entries.js
@@ -1,7 +1,39 @@
+import { Box } from "@chakra-ui/react";
 import React from "react";
+import Entry from "./Entry";
 
 const Entries = () => {
-  return <div></div>;
+  const data = {
+    data: [
+      {
+        id: "176",
+        rating: 0,
+        lastPracticeDate: new Date(),
+      },
+      {
+        id: "89",
+        rating: 2,
+        lastPracticeDate: new Date(),
+      },
+      {
+        id: "53",
+        rating: 1,
+        lastPracticeDate: new Date(),
+      },
+      {
+        id: "23",
+        rating: 3,
+        lastPracticeDate: new Date(),
+      },
+    ],
+  };
+  return (
+    <Box>
+      {data?.data.map((entry) => {
+        return <Entry key={entry.id} {...entry} />;
+      })}
+    </Box>
+  );
 };
 
 export default Entries;
