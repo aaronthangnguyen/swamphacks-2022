1fbee8bb9b1a91294522107a81bf0bbc3c125c83
yagyamalik <56045905+yagyamalik@users.noreply.github.com>
2022-01-29 17:00:43 -0500

    Added patch and delete

--- litkode-backend/playground.py
+++ litkode-backend/playground.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import json
 import pandas as pd
 import requests
@@ -6,7 +7,13 @@ import sqlite3
 from sqlite3 import Error
 from fastapi import FastAPI, UploadFile, File, Form
 from fastapi.middleware.cors import CORSMiddleware
+from typing import Optional
+from pydantic import BaseModel
 app = FastAPI()
+class Item(BaseModel):
+    id: str
+    rating: Optional[int] = 0
+    lastPracticeDate: Optional[datetime] = None
 def connect_to_db(db_file):
     """
     Connect to an SQlite database, if db file does not exist it will be created
@@ -63,5 +70,23 @@ async def write_data(id: str):
     conn.commit()
     print('SQL insert process finished')
 
-# @app.patch("/api/questions/{id}")
-# async def patch_data(id: str):
+@app.patch("/api/questions/{id}")
+async def create_data(item:Item):
+    item_id = item.id
+    item_rating = item.rating
+    item_lastPracticeDate = item.lastPracticeDate
+    c = conn.cursor()
+    sql = '''UPDATE User SET rating = ?, lastPracticeDate = ? WHERE id = ?'''
+    val = (item_rating,item_lastPracticeDate,item_id)
+    c.execute(sql,val)
+    conn.commit()
+    print('SQL insert process finished')
+
+@app.delete("/api/questions/{id}")
+async def delete_item(id: str):
+    c = conn.cursor()
+    sql = '''DELETE FROM User WHERE id = ?'''
+    c.execute(sql,(id,))
+    
+
+
