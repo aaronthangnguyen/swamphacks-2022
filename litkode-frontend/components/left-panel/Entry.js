9a14efcd7aecb7f664929826f4ac2a66c250f95f
Aaron Nguyen <me@aaronnguyen.dev>
2022-01-29 11:27:02 -0500

    Add icons to Entry

--- litkode-frontend/components/left-panel/Entry.js
+++ litkode-frontend/components/left-panel/Entry.js
@@ -1,4 +1,16 @@
-import { Box, Collapse, Flex, Text, useDisclosure } from "@chakra-ui/react";
+import {
+  Box,
+  Collapse,
+  Flex,
+  Text,
+  useDisclosure,
+  IconButton,
+} from "@chakra-ui/react";
+import {
+  ExternalLinkIcon,
+  ChevronDownIcon,
+  ChevronUpIcon,
+} from "@chakra-ui/icons";
 import React from "react";
 import Rating from "./Rating";
 
@@ -15,15 +27,38 @@ const Entry = () => {
         _hover={{
           boxShadow: "md",
         }}
-        onMouseEnter={onToggle}
-        onMouseLeave={onToggle}
       >
-        <Flex justify="space-between" align="center">
-          <p>ID Title Difficulty Rating</p>
+        <Flex justify="space-between" align="center" gap={1}>
+          <Text width="6rem">ID</Text>
+          <Text fontWeight="medium" flex={1}>
+            Title
+          </Text>
+          <Text width="8rem">Difficulty</Text>
+          <Text width="8rem">Rating</Text>
+          <IconButton
+            size="sm"
+            aria-label="Toggle collapse"
+            icon={isOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
+            onClick={onToggle}
+          />
+          <a
+            href="https://leetcode.com/"
+            rel="noreferrer noopener"
+            target="_blank"
+          >
+            <IconButton
+              size="sm"
+              aria-label="Go to Leetcode question"
+              icon={<ExternalLinkIcon />}
+            />
+          </a>
         </Flex>
         <Collapse in={isOpen} animateOpacity>
+          <Flex mt="1rem">
+            <Text>Topics</Text>
+          </Flex>
           <Flex justify="space-between" align="baseline">
-            <Text fontWeight="medium">Last Practice: [Date]</Text>
+            <Text color="gray.500">Last Practice: [Date]</Text>
             <Rating />
           </Flex>
         </Collapse>
