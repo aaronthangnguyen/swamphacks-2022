1e6ea072da216bcd91f0b5d7528553614668e752
Aaron Nguyen <me@aaronnguyen.dev>
2022-01-29 13:18:25 -0500

    Add global fetcher

--- litkode-frontend/pages/_app.js
+++ litkode-frontend/pages/_app.js
@@ -1,12 +1,17 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../styles/theme";
 import "../styles/global.css";
+import { SWRConfig } from "swr";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <SWRConfig
+      value={{ fetcher: (url) => fetch(url).then((res) => res.json()) }}
+    >
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SWRConfig>
   );
 }
 
