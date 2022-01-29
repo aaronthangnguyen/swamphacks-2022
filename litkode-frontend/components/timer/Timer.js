5b946aba32cda05eb62a4a241a2c6ac729f9ac8c
Aaron Nguyen <me@aaronnguyen.dev>
2022-01-29 09:44:36 -0500

    Add timer functionality

--- litkode-frontend/components/timer/Timer.js
+++ litkode-frontend/components/timer/Timer.js
@@ -1,23 +1,34 @@
-import { Button, Box, Center, Text, Flex } from "@chakra-ui/react";
+import { Button, Box, Text, Flex } from "@chakra-ui/react";
 import React from "react";
+import { useStopwatch } from "react-timer-hook";
 
 const Timer = () => {
+  const { seconds, minutes, isRunning, start, pause, reset } = useStopwatch({
+    autoStart: false,
+  });
+  const handleReset = () => {
+    reset();
+    pause();
+  };
   return (
     <Box>
       <Text fontWeight="bold" fontSize="3xl" display="block" textAlign="center">
-        00:00
+        {String(minutes).padStart(2, "0")}:{String(seconds).padStart(2, "0")}
       </Text>
       <Flex gap="0.5rem">
         <Button
           bgColor="orange.400"
           color="white"
           width="50%"
+          onClick={isRunning ? pause : start}
           _hover={{ bgColor: "orange.500" }}
           _active={{ bgColor: "orange.600" }}
         >
-          Start
+          {isRunning ? "Pause" : "Start"}
         </Button>
-        <Button width="50%">Reset</Button>
+        <Button width="50%" onClick={handleReset}>
+          Reset
+        </Button>
       </Flex>
     </Box>
   );
