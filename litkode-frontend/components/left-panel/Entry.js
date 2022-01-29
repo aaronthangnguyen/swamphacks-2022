48135ac80193b028c40c438566cc320fedd7eec7
Aaron Nguyen <me@aaronnguyen.dev>
2022-01-29 10:52:33 -0500

    Allow Entry to collapse on hover

--- litkode-frontend/components/left-panel/Entry.js
+++ litkode-frontend/components/left-panel/Entry.js
@@ -1,31 +1,34 @@
-import { Box, Select, Flex } from "@chakra-ui/react";
+import { Box, Collapse, Flex, Text, useDisclosure } from "@chakra-ui/react";
 import React from "react";
-import { ReactionBarSelector } from "@charkour/react-reactions";
+import Rating from "./Rating";
 
 const Entry = () => {
+  const { isOpen, onToggle } = useDisclosure();
   return (
-    <Box
-      background="gray.50"
-      rounded="md"
-      px="1rem"
-      py="0.5rem"
-      mt="0.5rem"
-      boxShadow="base"
-      _hover={{
-        boxShadow: "md",
-      }}
-    >
-      <Flex justify="space-between" align="center">
-        <p>ID Title Difficulty Rating</p>
-        <ReactionBarSelector
-          reactions={[
-            { label: "Easy", node: <div>😎</div> },
-            { label: "Medium", node: <div>😌</div> },
-            { label: "Hard", node: <div>😭</div> },
-          ]}
-        />
-      </Flex>
-    </Box>
+    <>
+      <Box
+        background="gray.50"
+        rounded="md"
+        p="1rem"
+        mt="0.5rem"
+        boxShadow="base"
+        _hover={{
+          boxShadow: "md",
+        }}
+        onMouseEnter={onToggle}
+        onMouseLeave={onToggle}
+      >
+        <Flex justify="space-between" align="center">
+          <p>ID Title Difficulty Rating</p>
+        </Flex>
+        <Collapse in={isOpen} animateOpacity>
+          <Flex justify="space-between" align="baseline">
+            <Text fontWeight="medium">Last Practice: [Date]</Text>
+            <Rating />
+          </Flex>
+        </Collapse>
+      </Box>
+    </>
   );
 };
 
