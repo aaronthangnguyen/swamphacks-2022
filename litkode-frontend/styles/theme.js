4ea4ef1f54fffa90949fc88aec829f0c91eafeed
Aaron Nguyen <me@aaronnguyen.dev>
2022-01-29 13:47:04 -0500

    Add medium font weight

--- litkode-frontend/styles/theme.js
+++ litkode-frontend/styles/theme.js
@@ -1,6 +1,7 @@
 import { extendTheme } from "@chakra-ui/react";
 import "@fontsource/ibm-plex-sans";
 import "@fontsource/ibm-plex-sans/500.css";
+import "@fontsource/ibm-plex-sans/600.css";
 import "@fontsource/ibm-plex-sans/700.css";
 
 const theme = extendTheme({
