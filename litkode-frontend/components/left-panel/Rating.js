817026d8f353e06fb91855088cc377f321ff68cf
Aaron Nguyen <me@aaronnguyen.dev>
2022-01-29 21:05:43 -0500

    Wire Rating to SWR API

--- litkode-frontend/components/left-panel/Rating.js
+++ litkode-frontend/components/left-panel/Rating.js
@@ -1,16 +1,37 @@
 import { Box } from "@chakra-ui/react";
 import { ReactionBarSelector } from "@charkour/react-reactions";
 import React from "react";
+import { useSWRConfig } from "swr";
+import ratings from "../data/ratings.json";
 
-const Rating = () => {
+const Rating = ({ id }) => {
+  const { mutate } = useSWRConfig();
+
+  const handleUpdate = async (label) => {
+    const rating = ratings[label];
+    await fetch(`http://localhost:8000/api/questions`, {
+      method: "PATCH",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        id: id,
+        rating: rating,
+        lastPracticeDate: new Date().toJSON(),
+      }),
+    });
+    mutate("http://localhost:8000/api/questions");
+  };
   return (
     <Box mb="0.25rem">
       <ReactionBarSelector
         reactions={[
-          { label: "Easy", node: <div>😎</div> },
+          { label: "Easy", value: "1", node: <div>😎</div> },
           { label: "Medium", node: <div>😌</div> },
           { label: "Hard", node: <div>😱</div> },
         ]}
+        onSelect={(label) => handleUpdate(label)}
       />
     </Box>
   );
