efb45909fb9348815a46c8c79d9e6ce64c8bf677
Aaron Nguyen <me@aaronnguyen.dev>
2022-01-29 19:47:12 -0500

    Allow CORS

--- litkode-backend/playground.py
+++ litkode-backend/playground.py
@@ -14,6 +14,19 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+    "https://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 class Item(BaseModel):
     id: str
